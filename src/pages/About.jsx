7ddf6946d932930c46e207e7594eb13a8378d25c
sugirangendahayo <sugirangendahayo835@gmail.com>
2026-04-22 15:28:42 +0200

    Add src/pages/About.jsx file

---
+++ src/pages/About.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="flex flex-col items-center justify-center ">
+      <h1 className="font-bold text-2xl">
+        About <span className="text-orange-600">us</span>
+      </h1>
+      <div className="text-center mx-[20%] py-8">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis
+          accusantium omnis qui harum dolores necessitatibus quasi iure tempore
+          provident aliquid possimus assumenda, voluptatibus earum expedita quis
+          sit ex tempora! At!
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam culpa aliquam eius distinctio vitae quidem eveniet ea minima dolorum dicta deserunt aliquid voluptatem rem quibusdam nisi, velit perspiciatis mollitia eaque?
+        Odio deserunt a sed consectetur consequatur. Eaque soluta mollitia quisquam rem hic cupiditate illo quidem esse, a labore, reprehenderit consequatur iusto laborum corporis dolores, natus ratione sapiente officia aliquam quis?
+        Iusto eaque saepe voluptatibus maiores. Quo ea, rerum fugit quis saepe repudiandae? Fuga autem, quia labore reiciendis magni temporibus numquam. Inventore quibusdam neque laudantium fugiat magni praesentium doloremque. Minima, ut.
+        Doloribus eveniet exercitationem necessitatibus odio consequuntur sit molestias ipsam dolore quos minima alias, veritatis voluptatem saepe asperiores magni temporibus voluptates ad quis ipsum nesciunt delectus. Voluptas neque nulla architecto modi.
+        Cum sint similique, recusandae consectetur ab nihil! Architecto asperiores repellendus nisi aspernatur ipsam autem ipsum ullam! Minus maiores quas aliquid molestiae tempore doloribus animi autem, vero repellendus earum voluptas eveniet!
+        Eaque commodi maiores minima optio, est dolor placeat sint sapiente atque nulla quos iusto temporibus aspernatur quo voluptate accusantium dolorum tempora necessitatibus consequatur quibusdam nesciunt quam accusamus ipsa sequi. Quas.
+        Tenetur sapiente incidunt adipisci. Voluptate cumque perferendis aut consectetur ipsa, quos, molestiae quo, eos ab accusamus aliquam? Maxime recusandae voluptatibus error eaque, nemo commodi numquam sit officiis magni, sed ex?
+        Blanditiis veritatis sit, doloremque aspernatur rem atque alias velit saepe! Alias voluptatibus officiis molestias quaerat fuga quis, distinctio, debitis perspiciatis, voluptas molestiae doloremque repellendus quo dolore dolorum nisi saepe quia!
+        Perspiciatis iusto, eos error dolore commodi cupiditate quam repellat amet sit libero sunt facilis ea aliquam, esse recusandae ratione dolorum, excepturi nisi. Earum natus, nulla magnam autem consequatur est. Numquam.
+        Beatae, nulla, alias mollitia id tempora eveniet numquam quia, minus reprehenderit vel labore cumque sunt soluta dignissimos natus quos laboriosam. Explicabo hic dolore nemo ratione earum, quo natus nihil necessitatibus?</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;

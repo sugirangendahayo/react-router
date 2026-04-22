42a41adec9ea973c538c4c84146bb67157c8becd
sugirangendahayo <sugirangendahayo835@gmail.com>
2026-04-22 15:29:06 +0200

    Add src/pages/Home.jsx file

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import van from "../assets/van.jpg";
+
+const Home = () => {
+  return (
+    <>
+      <div
+        className="relative h-[70vh] bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: `url(${van})` }}
+      >
+        <div className="absolute inset-0 bg-black/50"></div>
+
+        <div className="relative z-10 flex flex-col gap-8 items-center justify-center h-full text-white p-4  text-center">
+          <h1 className="text-4xl md:text-6xl font-bold">You got the travel plans, we got the travel vans</h1>
+          <Link className="p-2 bg-amber-600 text-white font-bold w-[90%] ">Find you van</Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
